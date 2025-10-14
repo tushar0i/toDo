4288a3ffdba432c75a911b81dbdf28e0f454dbf0
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-10-15 01:21:54 +0530

    basic toDo structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [todos , settoDos] = useState([{
+        title : "running",
+        description : "moring from 6 to 7 am",
+        completed : true
+    },{
+        title : "maths assignment",
+        description : "from 8 - 10 am",
+        completed : false
+    },{
+        title : "python",
+        description : "complete two topics/chapter from freecodecamp video",
+        completed : false
+    }])
+
+    function addtoDo(){
+        
+        settoDos([...todos,{//this helps us add thing in the end of orignal array
+            title : "task title",
+            description : "task description"
+        }])
+    }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <div>
+            <button onClick={addtoDo}>add task templet</button>
+            {todos.map(function(todo){
+                return <Todo title={todo.title} description={todo.description} completed={todo.completed} />
+            })}
+        </div>
+        
   )
+
+  function Todo(state){
+    return <div>
+        <h2>{state.title}</h2>
+        <h3>{state.description}</h3>
+        {/* <button>somethign</button> */}
+    </div>
+  }
 }
 
 export default App
