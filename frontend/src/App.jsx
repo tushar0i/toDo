3e53b49b6480c32ae113a4b133cfbbb431726205
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-10-16 23:59:49 +0530

    just moving things

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+        hi everyone this is tushar toDo app made with love alongside harkirat sir
+    </div>
+  )
+}
+
+export default App
