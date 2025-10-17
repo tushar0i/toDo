617b9986139ab2f21d26d4db539a9d889f17122d
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-10-17 17:07:37 +0530

    Connecting frontend with backend

--- frontend/components/CreateTodo.jsx
+++ frontend/components/CreateTodo.jsx
@@ -1,8 +1,40 @@
-export function CreateTodo(){
+import { useState } from "react"
+
+
+export function CreateTodo() {
+    // we will eventualy use react-query for getting the inner html but for now we
+    // creating local state variable which is an unoptimal way of doing this
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+
+    // rule of thumb in react is to minimize the number of rerenders 
+
+
     return <div>
-        <input type="text" placeholder="title"/> <br />
-        <input type="text" placeholder="description"/> <br />
-        <button>Add a toDo</button>
+        <input type="text" placeholder="title" id="title" onChange={(e) => {
+            const value = e.target.value
+            setTitle(value)
+        }} /> <br />
+        <input type="text" placeholder="description" id="description" onChange={(e) => {
+            const value = e.target.value
+            setDescription(value)
+        }} /> <br />
+        <button id="btnatd" onClick={() => {
+            fetch("http://localhost:3000/todo", { // this thing will be easier when we use axios library
+                method: "POST",
+                body: JSON.stringify({
+                    title: title,
+                    description: description
+                }),
+                headers: {
+                    "Content-type": "application/json"
+                }
+            })
+                .then(async function (response) {
+                    const data = await response.json()
+                    alert("Task added")
+                })
+        }}>Add a toDo</button>
     </div>
 }
- 
+
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,33 @@
 import './App.css'
+import { CreateTodo } from '../components/CreateTodo'
+import { Todos } from '../components/Todos'
+import { useEffect, useState } from 'react'
 
 function App() {
-  return (
-    <div>
-        hi everyone this is tushar toDo app made with love alongside harkirat sir
-    </div>
-  )
+    const [todos, setTodos] = useState([])
+    // fetch("http://localhost:3000/todo") // we can't make App function async that's why we have to use .then 
+    // .then(async (response)=> {
+    //     const data = await response.json()   
+    //     setTodos(data.todos)    
+    // }) 
+    // this is not the right way to fetch data from backend 
+    // a infinite loop of fetch request's
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("http://localhost:3000/todo")
+            const data = await response.json()
+            setTodos(data.todos)
+        }
+        fetchData()
+    }, []) // The empty array [] ensures this effect runs only once
+
+    return (
+        <div>
+            <CreateTodo></CreateTodo>
+            <Todos todos={todos}></Todos>
+        </div>
+    )
 }
 
 export default App
